e85728e4102098a80636da092232a1d0b3e4f00a
Johannes Schuitemaker <johannes.schuitemaker@vened.nl>
2018-10-07 11:37:30 +0200

    x

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -16,19 +16,20 @@ oc project ${GUID}-nexus
 oc new-app -f ./Infrastructure/templates/nexus.yaml -p GUID=${GUID}
 sleep 10
 
+while : ; do
+  echo "Checking if Nexus is Ready..."
+  oc get pod -n ${GUID}-nexus|grep -v running|grep "1/1"
+  [[ "$?" == "1" ]] || break
+  echo "...no. Sleeping 10 seconds."
+  sleep 10
+done
+
 # setup nexus: would like to do this using a Hook but couldn't find how 
 curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
 chmod +x setup_nexus3.sh
 sh setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}' -n ${GUID}-nexus )
 rm -f setup_nexus3.sh
 
-while : ; do
-  echo "Checking if Nexus is Ready..."
-  oc get pod -n ${GUID}-nexus|grep '\-2\-'|grep -v deploy|grep "1/1"
-  [[ "$?" == "1" ]] || break
-  echo "...no. Sleeping 10 seconds."
-  sleep 10
-done
 
 # this will set the default URL on the openshift console to point to the Nexus console instead of the registry
 oc annotate route nexus3 console.alpha.openshift.io/overview-app-route=true
