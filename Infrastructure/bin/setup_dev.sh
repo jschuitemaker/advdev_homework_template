ac36373850cefa634bb1a916a989ad7e0eaabae2
Johannes Schuitemaker <johannes.schuitemaker@vened.nl>
2018-10-10 16:06:47 +0200

    nationalparks

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -15,25 +15,17 @@ oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n
 # Set up a MongoDB database in the development project. (see README in MLBPARKS)
 oc new-app -e MONGODB_USER=mongodb -e MONGODB_PASSWORD=mongodb -e MONGODB_DATABASE=parks -e MONGODB_ADMIN_PASSWORD=mongodb --name=mongodb registry.access.redhat.com/rhscl/mongodb-34-rhel7:latest -n ${GUID}-parks-dev
 
-
-# Create two OpenShift projects for development (including test) and production.
-
-# To conserve resources, you need to test on the development images of the application, rather than setting up a separate test/QA project.
-
-# Set up the correct permissions for the production project to deploy images from the development project.
-
-# Set up a replicated MongoDB database (StatefulSet) with at least three replicas in the production project.
-
+# MLBPARKS--------------
 # Create build configurations in the development project.
 # TODO - Will need to change the imagestream name back to jboss-eap70-openshift:1.7
 oc new-build --binary=true --name="mlbparks" wildfly-120-centos7 -n ${GUID}-parks-dev
-#oc new-build --binary=true --name="nationalparks" wildfly-120-centos7 -n ${GUID}-parks-dev
 
 # Create deployment configurations in both the development and production projects.
 oc new-app ${GUID}-parks-dev/mlbparks:0.0-0 --name=mlbparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
 
 # Configure the applications using ConfigMaps.
-oc create configmap mlbparks-config --from-env-file=./Infrastructure/environments/MLBParks-dev.env -n ${GUID}-parks-dev
+cat ./Infrastructure/environments/MongoDB-dev.env  <(echo) ./Infrastructure/environments/MLBParks-dev.env > ./MLBParks-dev.map
+oc create configmap mlbparks-config --from-env-file=./MLBParks-dev.map -n ${GUID}-parks-dev
 oc set env dc/mlbparks --from=configmap/mlbparks-config -n ${GUID}-parks-dev
 oc set probe dc/mlbparks --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-dev
 oc set probe dc/mlbparks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
@@ -47,3 +39,29 @@ oc expose svc/mlbparks --labels=type=parksmap-backend -n ${GUID}-parks-dev
 # The endpoint `/ws/data/load/` creates the data in the MongoDB database and will need to be called (preferably with a post-deployment-hook)
 # once the Pod is running.
 oc set deployment-hook dc/mlbparks  -n ${GUID}-parks-dev --post -c mlbparks --failure-policy=abort -- curl http://$(oc get route mlbparks -n ${GUID}-parks-dev -o jsonpath='{ .spec.host }')/ws/data/load/
+
+# NATIONALPARKS--------------
+# Create build configurations in the development project.
+# TODO - Will need to change the imagestream name back to redhat-openjdk18-openshift:1.2
+oc new-build --binary=true --name="nationalparks" redhat-openjdk18-openshift:latest -n ${GUID}-parks-dev
+
+# Create deployment configurations in both the development and production projects.
+oc new-app ${GUID}-parks-dev/nationalparks:0.0-0 --name=nationalparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
+
+# Configure the applications using ConfigMaps.
+cat ./Infrastructure/environments/MongoDB-dev.env  <(echo) ./Infrastructure/environments/Nationalparks-dev.env > ./Nationalparks-dev.map
+oc create configmap nationalparks-config --from-env-file=./Nationalparks-dev.map -n ${GUID}-parks-dev
+
+oc set env dc/nationalparks --from=configmap/nationalparks-config -n ${GUID}-parks-dev
+oc set probe dc/nationalparks --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-dev
+oc set probe dc/nationalparks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
+
+# this will create the services
+oc expose dc/nationalparks --port 8080 -n ${GUID}-parks-dev
+
+# expose the above service as a route and mark it as type 'parksmap-backend' so the UI can discover this service
+oc expose svc/nationalparks --labels=type=parksmap-backend -n ${GUID}-parks-dev
+
+# The endpoint `/ws/data/load/` creates the data in the MongoDB database and will need to be called (preferably with a post-deployment-hook)
+# once the Pod is running.
+oc set deployment-hook dc/nationalparks -n ${GUID}-parks-dev --post -c nationalparks --failure-policy=abort -- curl http://$(oc get route nationalparks -n ${GUID}-parks-dev -o jsonpath='{ .spec.host }')/ws/data/load/
